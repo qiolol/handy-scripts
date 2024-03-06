e4f4ec4590a936aa14ea045c265cfc829163a5cb
qiolol <6equj5@tuta.io>
2024-03-05 20:52:20 -0700

    Use only alphanumeric characters instead of Base64; add output options

--- randstr.sh
+++ randstr.sh
@@ -1,37 +1,117 @@
 #!/usr/bin/env bash
-# prints random Base64 ASCII chars (how many specified in $1)
 
-# checks number and format of args
-function check_input() {
-    # correct number of args
-    if [[ "${#}" = 1 ]]
+# Prints a random string
+
+function print_usage()
+{
+    echo -e "Usage: ${0} [OPTIONS] <length>\n\n\
+DESCRIPTION
+\tPrints a random alphanumeric string
+PARAMETERS
+\tlength
+\t\tThe number of characters in the string
+OPTIONS
+\t-a
+\t\tUse only alphabetic characters (upper- and lowercase letters)
+\t-d
+\t\tUse only numeric characters
+\t-n
+\t\tDon't print a newline after the string"
+}
+
+function parse_options()
+{
+    while getopts "adn" NAME
+    do
+        case "${NAME}" in
+            a) readonly "${MODE_ALPHABETIC:=true}" ;;
+            d) readonly "${MODE_NUMERIC:=true}" ;;
+            n) readonly "${NO_NEWLINE:=true}" ;;
+            ?) return 1 ;;
+        esac
+    done
+
+    # Since they're mutually exclusive, only one of the optional modes should
+    # have been chosen.
+    if [[ -v MODE_ALPHABETIC &&
+          -v MODE_NUMERIC ]]
     then
-        # correct arg format
-        if [[ "${1}" =~ ^[0-9]+$ ]]
-        then
-            return 0
-        fi
+        return 1
     fi
-    
-    return 1
+
+    return 0
 }
 
-function print_usage() {
-    echo "usage: randstr <number_of_chars>"
+function parse_parameters()
+{
+    local -r REQUIRED_PARAMS=1
+    local -r MAX_PARAMS=1
+
+    if (( ${#} < REQUIRED_PARAMS ||
+          ${#} > MAX_PARAMS ))
+    then
+        return 1
+    fi
+
+    if [[ "${1}" =~ ^[[:digit:]]+$ ]]
+    then
+        readonly LENGTH="${1}"
+    fi
+
+    # Check for required parameter.
+    if [[ ! -v LENGTH ]]
+    then
+        return 1
+    fi
+
+    return 0
 }
 
-# prints random strings
-function randstr() {
-    echo "$(base64 -w 0 /dev/urandom | head -c "${1}")"
+function parse_arguments()
+{
+    if ! parse_options "${@}"
+    then
+        return 1
+    fi
+
+    shift $((OPTIND-1))
+
+    if ! parse_parameters "${@}"
+    then
+        return 1
+    fi
+
+    return 0
 }
 
-if check_input "${@}"
+function randstr()
+{
+    local MODE="[:alnum:]" # Alphanumeric by default
+
+    # Apply optional mode
+    if [[ -v MODE_ALPHABETIC ]]
+    then
+        MODE="[:alpha:]"
+    elif [[ -v MODE_NUMERIC ]]
+    then
+        MODE="[:digit:]"
+    fi
+
+    head -c "${LENGTH}" <(tr -dc "${MODE}" < /dev/urandom)
+
+    if [[ ! -v NO_NEWLINE ]]
+    then
+        echo
+    fi
+}
+
+if ! parse_arguments "${@}"
 then
-    randstr "${1}"
-    
-    exit 0
-else
     print_usage
-    
+
     exit 1
 fi
+
+randstr
+
+exit 0
